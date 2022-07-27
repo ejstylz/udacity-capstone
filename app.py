c8b9079b6f6acbb4940a82cdc9571f80e95e7ab0
Theophilus Okwugwuni <theophilusdev@gmail.com>
2022-07-28 00:14:04 +0100

    fix build error

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 from flask import Flask
-import logging
+from flask.logging import create_logger
 
 app = Flask(__name__)
 
+LOG = create_logger(app)
+LOG.setLevel(logging.INFO)
+
 @app.route("/")
 def hello():
-    app.logger.info('request successful')
+    LOG.info("request successful")
     return "Theo's capstone"
 
 if __name__ == "__main__":
-    ## stream logs to a file
-    logging.basicConfig(filename='app.log',level=logging.DEBUG)
-    app.run(host='0.0.0.0', port=8080)
+    app.run()
